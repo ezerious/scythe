2cc9dec74708687e3569d6045db9e0e5e39545d5
Ezra <edantowitz1@gmail.com>
2020-11-28 19:02:56 -0500

    Add Invaders Expansion. Correct for anti-ban bias
    Adds support for factions and player mats for Invaders from Afar (Togawa/Albion and Innovative/Militant).
    Corrects for mild bug that biased against expansions that could have a banned faction. Previously it would re-roll factions and player mats if there was a banned combo. But now it selects all factions first, and then player mats. If any player mats result in a banned combo it will re-roll the player mats to match with the factoins.

--- scythe.py
+++ scythe.py
@@ -1,5 +1,6 @@
 '''
-Randomly selects factions/economy boards for N players in Scythe
+Randomly selects factions/economy boards for N players in Scythe. 
+Second optional parameter allows to use Invaders Factions and Player Mats even if fewer than 6 players are playing.
 '''
 import sys
 import random
@@ -9,36 +10,64 @@ def main():
 		print("Invalid input");
 		return;
 	N = int(sys.argv[1]);
-	if N > 5 or N < 1:
+	if len(sys.argv) > 2:
+	# If second argument set to y/Y expansion factions/players will be used
+		if 'y' == str(sys.argv[2]).lower():
+			N = 7
+	if N > 7 or N < 1:
 		print("Invalid input");
 		return;
 	players = get_combos(N)
-	print(players)
+	count = int(sys.argv[1]);
+	print(players[:count])
 	
 
 def get_combos(n):
-	#randomly generates n pairings of factions/player mats. Recursively calls if finds invalid pairing
-	factions = ['Polania', 'Nordic', 'Rusviet', 'Saxony', 'Crimea'];
-	playerMats = ['Agricultural', 'Engineering', 'Industrial', 'Mechanical', 'Patriotic'];
-	playerNums = list(range(5));
-	random.shuffle(playerNums);
-	factionNums = list(range(5));
-	random.shuffle(factionNums);
+	#randomly generates n pairings of factions/player mats. Recalls player mats if any combo is invalid
+	factions = get_faction_mats(n);
+	playerMats = get_player_mats(n)
 
-	combos = []
-	for i in range(n):
-		val = factions[factionNums[i]] +'|'+playerMats[playerNums[i]];
-		combos.append(val);
-	if bad_combos(combos):
-		combos = get_combos(n);
-		return combos
+	while True:
+		combos = []
+		for i in range(n):
+			val = factions[i] +'|'+playerMats[i];
+			combos.append(val);
+		if bad_combos(combos):
+			playerMats = get_player_mats(n);
+		else:
+			return combos;
+	return []
+
+def get_player_mats(n):
+	if n <= 5:
+		playerMats = ['Agricultural', 'Engineering', 'Industrial', 'Mechanical', 'Patriotic'];
 	else:
-		return combos;
+		playerMats = ['Agricultural', 'Engineering', 'Industrial', 'Mechanical', 'Patriotic', 'Innovative', 'Militant'];
+
+	playerNums = list(range(len(playerMats)));
+	random.shuffle(playerNums);
+	playerList = []
+	for i in range(n):
+		playerList.append(playerMats[playerNums[i]])
+	return playerList
+	
+def get_faction_mats(n):
+	#randomly generates n factions 
+	if n <= 5:
+		factions = ['Polania', 'Nordic', 'Rusviet', 'Saxony', 'Crimea'];
+	else:
+		factions = ['Polania', 'Nordic', 'Rusviet', 'Saxony', 'Crimea', 'Albion', 'Togawa'];
+	factionNums = list(range(len(factions)));
+	random.shuffle(factionNums);
+	factionList = []
+	for i in range(n):
+		factionList.append(factions[factionNums[i]])
+	return factionList
 
 def bad_combos(combos):
 	# checks for any bad combos. Returns true if any found
 	for val in combos:
-		if val == 'Rusviet|Industrial' or val == 'Crimea|Patriotic':
+		if val == 'Rusviet|Industrial' or val == 'Crimea|Patriotic' or val == 'Crimea|Innovative' or val == 'Crimea|Militant' or val == 'Rusviet|Innovative' or val == 'Rusviet|Militant':
 			return True;
 	return False;
 
